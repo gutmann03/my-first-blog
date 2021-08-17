c7a30d5a280d1cbf3601267d886a15686d194cef
Volodymyr Smetaniuk <gutmann003@gmail.com>
2021-08-17 22:52:58 +0300

    third commit, tho pages

--- src/mysite/blog/urls.py
+++ src/mysite/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .models import Post
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('home/', views.get_menu, name='menu'),
 ]
