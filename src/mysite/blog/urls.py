fb45d16bf728ca6753e2e840c63d2e8fff7cf329
Volodymyr Smetaniuk <gutmann003@gmail.com>
2021-08-18 21:31:31 +0300

    fourth commit cool site

--- src/mysite/blog/urls.py
+++ src/mysite/blog/urls.py
@@ -5,4 +5,5 @@ from .models import Post
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('menu/', views.get_menu, name='menu'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
